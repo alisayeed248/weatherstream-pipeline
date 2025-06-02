0fcc3e4a981b27af0cc6cdd843b037997887c498
Sayeed Ali <alisayeed248@gmail.com>
2025-06-02 17:11:42 -0400

    Added main weather implementation.

---
+++ src/main/java/com/weatherstream/model/Main.java
@@ -0,0 +1,24 @@
+package com.weatherstream.model;
+
+public class Main {
+  private double temp;
+  private double humidity;
+
+  // Getters and setters
+  public double getTemp() {
+    return temp;
+  }
+
+  public void setTemp(double temp) {
+    this.temp = temp;
+  }
+
+  public void setHumidity(double humidity) {
+    this.humidity = humidity;
+  }
+
+  public double getHumidity() {
+    return humidity;
+  }
+
+}
