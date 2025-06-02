fa7cc3e85b2469ae7467e9f596ce412b463e9832
Sayeed Ali <alisayeed248@gmail.com>
2025-06-02 17:02:59 -0400

    Fixed issue where setters didn't have parameters.

---
+++ src/main/java/com/weatherstream/model/WeatherData.java
@@ -0,0 +1,23 @@
+package com.weatherstream.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class WeatherData {
+	private String name;
+	private Main main;
+	private Weather[] weather;
+
+	// Getters and setters
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Main getMain() { return main; }
+	public void setMain(Main main) { this.main = main; }
+
+	public Weather[] getWeather() { return weather; }
+	public void setWeather(Weather[] weather) { this.weather = weather; }
+}
