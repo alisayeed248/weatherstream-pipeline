601e6fa426e1d9a7188b95a98e46607feb8263dc
Sayeed Ali <alisayeed248@gmail.com>
2025-06-04 19:46:25 -0400

    Added consumer service base code.

---
+++ weather-consumer-service/src/main/java/com/weatherstream/consumer/WeatherConsumerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.weatherstream.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherConsumerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherConsumerServiceApplication.class, args);
+	}
+
+}
---
+++ weather-consumer-service/src/test/java/com/weatherstream/consumer/WeatherConsumerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weatherstream.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherConsumerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
